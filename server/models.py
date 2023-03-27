5b5fbadcc3ecf8b12ace64a02a316d284b484913
Justin Elue <mjuelue@gmail.com>
2023-03-27 14:01:56 -0500

    a brighter tomorrow

--- server/models.py
+++ server/models.py
@@ -1,10 +1,80 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+from config import db
 
-db = SQLAlchemy()
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+# db = SQLAlchemy(metadata= MetaData())
+
+# Models go here!
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    # serialize_rules = ()
 
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String)
+    biography = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
 
-# add any models you may need. 
+    books = db.relationship("Book", backref = "author")
+
+class Genre(db.Model, SerializerMixin):
+    __tablename__ = 'genres'
+
+    # serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    # serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    full_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    liked_books = db.Column(db.Integer, db.ForeignKey("books.id"))
+class UserBook(db.Model, SerializerMixin):
+    __tablename__ = 'user_books'
+
+    # serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    # serialize_rules = ()
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    price = db.Column(db.Float)  #use random float number snap to 2
+    isbn = db.Column(db.Integer)
+    likes = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    
+    genre_id = db.Column(db.Integer, db.ForeignKey("genres.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+
--- server/seed.py
+++ server/seed.py
@@ -1,50 +1,144 @@
-from random import choice as rc
+#!/usr/bin/env python3
 
+# Standard library imports
+from random import randint, choice as rc
+
+
+# Remote library imports
+from faker import Faker
+from faker.providers import isbn, person, simple_profile    
+# Local imports
 from app import app
-from models import db, Hero, Power, HeroPower
+from models import Author, Genre, User, UserBook, Book, db
+
+fake = Faker()
+
+def make_Genre():
+    
+    Genre.query.delete()
+
+    genre_obj = ['Drama', 'Fable', 'Fiction', 'Folklore', 'Historical Fiction', 'Horror', 'Mystery',
+    'Poetry', 'Science Fiction']
+
+    for i in range(len(genre_obj)):
+    # for i in range(10):
+        genre = Genre(
+            genre = genre_obj[i] ,
+               
+        )
+
+        genre_obj.append(genre)
+    
+    db.session.add_all(genre_obj)
+    db.session.commit()
+
+def make_author():
+
+    Author.query.delete()
+
+    authors = ["Branden Sanderson", "James Patterson", "Ruth Benedict", 
+    "bell hooks", "Laura Purcell", "Action Bronson", "Mark Twain", "Brent Weeks", 
+    "Andrew Loomis", "Coleen Hoover", "Chris Bohjalian", "Chuck Tingle", "Blanca Lepinska",
+    "Khaled Hosseini", "Chimamanda Ngozi Adichie", "Fonda Lee", "Lucy Maud Montgomery",
+    "Hanya Yanagihara", "Toshikazu Kawaguchi", "Susan Hill", "Agatha Christie", "Mary Shelley",
+    "Neal Stevenson"]
+
+
+    author_obj = []
+
+    for i in range(len(authors)):
+    # for i in range(10):
+    
+        author = Author(
+            full_name= authors[i],
+            biography= fake.text()
+        )
+
+        author_obj.append(author)
+
+
+    
+    db.session.add_all(author_obj)
+    db.session.commit()
+
+# tuna = Fish(name='Tuna', season='year round', price=100, location_id=13, bait_id=4)
+# session.bulk_save_objects([smallmouth_bass, shad, bream, catfish, sunfish, tiger_trout, lingcod, pike, flounder, eel, herring, red_snapper, pufferfish, sardine, albacore, super_cucumber, red_mullet, squid, pike, chub, dorado, salmon, midnight_carp, bullhead, large_mouth_bass, sturgeon, walleye, perch, ghostfish, stonefish, ice_pip, lava_eel, woodskip, sandfish, scorpion_carp, green_algae, white_algae, slimejack, void_salmon, blue_discus, lionfish, tilapia, stingray, tuna]) 
+# session.commit()  
+
+def make_book():
+    
+    Book.query.delete()
+
+    books_obj = []
+
+    
+
+    for i in range(23):
+    # for i in range(10):
+    
+        book = Book(
+            title= fake.name(),
+            price= randint(0,999),
+            isbn= fake.isbn(),
+            likes= randint(),
+            genre_id= randint(),
+            # author_id= randint(1,23)
+            author_id= i
+        )
+
+        books_obj.append(book)
+    
+    db.session.add_all(books_obj)
+    db.session.commit()
+
+def make_user():
+    
+    User.query.delete()
+
+    users_obj = []
+
+    for i in range(23):
+    # for i in range(10):
+    
+        user = User(
+            password= randint() ,
+            username= fake.person(),
+            full_name= fake.name(),   
+        )
+
+        users_obj.append(user)
+    
+    db.session.add_all(users_obj)
+    db.session.commit()
+
+def make_users_books():
+    
+    User.query.delete()
+
+    users_books_obj = []
+
+    for i in range(23):
+    # for i in range(10):
+    
+        user_books = UserBook(
+            user_id = randint(1,23),
+            book_id = randint(1,23)  
+        )
+
+        users_books_obj.append(user_books)
+    
+    db.session.add_all(users_books_obj)
+    db.session.commit()
+
+
+
 
 if __name__ == '__main__':
+    fake = Faker()
     with app.app_context():
-        print("Clearing db...")
-        Power.query.delete()
-        Hero.query.delete()
-        HeroPower.query.delete()
-
-        print("Seeding powers...")
-        powers = [
-            Power(name="super strength", description="gives the wielder super-human strengths"),
-            Power(name="flight", description="gives the wielder the ability to fly through the skies at supersonic speed"),
-            Power(name="super human senses", description="allows the wielder to use her senses at a super-human level"),
-            Power(name="elasticity", description="can stretch the human body to extreme lengths"),
-        ]
-
-        db.session.add_all(powers)
-
-        print("Seeding heroes...")
-        heroes = [
-            Hero(name="Kamala Khan", super_name="Ms. Marvel"),
-            Hero(name="Doreen Green", super_name="Squirrel Girl"),
-            Hero(name="Gwen Stacy", super_name="Spider-Gwen"),
-            Hero(name="Janet Van Dyne", super_name="The Wasp"),
-            Hero(name="Wanda Maximoff", super_name="Scarlet Witch"),
-            Hero(name="Carol Danvers", super_name="Captain Marvel"),
-            Hero(name="Jean Grey", super_name="Dark Phoenix"),
-            Hero(name="Ororo Munroe", super_name="Storm"),
-            Hero(name="Kitty Pryde", super_name="Shadowcat"),
-            Hero(name="Elektra Natchios", super_name="Elektra"),
-        ]
-
-        db.session.add_all(heroes)
-
-        print("Adding powers to heroes...")
-        strengths = ["Strong", "Weak", "Average"]
-        hero_powers = []
-        for hero in heroes:
-            power = rc(powers)
-            hero_powers.append(
-                HeroPower(hero=hero, power=power, strength=rc(strengths))
-            )
-        db.session.add_all(hero_powers)
-        db.session.commit()
-
-        print("Done seeding!")
+        print("Starting seed...")
+        # Seed code goes here!
+        make_author()
+        make_book()
+        make_users_books()
+        make_user()
